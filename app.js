3613d8d462a220d0a4cc0417293037e44a942bc4
yarfed <yarfed@yandex.ru>
2018-12-14 02:59:14 +0300

    some code

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("fuck");
+var noble = require('noble');
+var peripheralIdOrAddress='00158700bc6f';
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+ 
+ noble.on('discover', function(peripheral) {
+  
+  if (peripheral.id === peripheralIdOrAddress || peripheral.address === peripheralIdOrAddress) {
+    console.log(peripheral);
+     
+ 
+    } 
+
+  });
+ 
+  
+ 
